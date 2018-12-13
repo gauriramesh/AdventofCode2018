4f9b3d3d6443ff94067aab86387a2464bdb8bd40
gauri <gauri.ramesh15@gmail.com>
2018-12-13 16:27:31 -0600

    finishes day 2 part 2

---
+++ day2-2.js
@@ -0,0 +1,24 @@
+var fs = require("fs");
+let data = fs.readFileSync("input/input02.txt").toString();
+let ids = data.split("\n");
+
+for(let i = 0; i < ids.length; i++) {
+    for(let j = 1; j < ids.length; j++) {
+        if(ids[i].length !== ids[j].length) {
+            continue;
+        }
+        let differenceCount = 0;
+        let differenceIndex = -1;
+        for(let k = 0; k < ids[i].length; k++) {
+            if(ids[i][k] !== ids[j][k]) {
+                differenceCount++;
+                differenceIndex = k;
+            }
+        }
+        if(differenceCount === 1) {
+            let answer =  ids[i].slice(0, differenceIndex) + ids[i].slice(differenceIndex + 1);
+            console.log(answer);
+            return answer;
+        }
+    }
+}
